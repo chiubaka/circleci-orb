f4abf086793bc03fbe3facc82c9495a16a0d2807
Daniel Chiu <daniel@chiubaka.com>
2022-08-11 16:01:00 +0800

    Fix bug in authenticate scripts where ~/.npmrc could not be resolved

--- src/scripts/authenticateWithNpm.sh
+++ src/scripts/authenticateWithNpm.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
 
-npmrc_path="${NPMRC_PATH:-"~/.npmrc"}"
+npmrc_path=${NPMRC_PATH:-~/.npmrc}
 
 echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" >> "$npmrc_path"
--- src/scripts/authenticateWithYarn.sh
+++ src/scripts/authenticateWithYarn.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
 
-npmrc_path="${NPMRC_PATH:-"~/.npmrc"}"
+npmrc_path=${NPMRC_PATH:-~/.npmrc}
 
 echo "//registry.yarnpkg.com/:_authToken=${NPM_TOKEN}" >> "$npmrc_path"
