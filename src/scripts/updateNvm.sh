a1b5c857cc2700c3d531103047377021e3e3fa1c
Daniel Chiu <daniel@chiubaka.com>
2022-08-24 17:00:36 +0800

    Resolve linting issues

--- src/scripts/updateNvm.sh
+++ src/scripts/updateNvm.sh
@@ -4,5 +4,7 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 
 # Load new NVM version into current shell
 export NVM_DIR="/opt/circleci/.nvm"
+# shellcheck disable=1090,1091
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+# shellcheck disable=1090,1091
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
