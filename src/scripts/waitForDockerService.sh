853125e98acb9336d42dc21fae86b7eae060aef2
Daniel Chiu <daniel@chiubaka.com>
2022-10-14 07:38:09 +0800

    Update test for waitForDockerService script

--- src/scripts/waitForDockerService.sh
+++ src/scripts/waitForDockerService.sh
@@ -3,7 +3,7 @@ set -e
 
 DOCKER_BINARY=${DOCKER_BINARY:-docker}
 
-NETWORK_NAME=`$DOCKER_BINARY container inspect "$CONTAINER_NAME" --format '{{range $net,$v := .NetworkSettings.Networks}}{{printf "%s\n" $net}}{{end}}'`
+NETWORK_NAME=`$DOCKER_BINARY container inspect "$CONTAINER_NAME" --format \'{{range $net,$v := .NetworkSettings.Networks}}{{printf "%s\n" $net}}{{end}}\'`
 
 $DOCKER_BINARY container run --network "$NETWORK_NAME" \
   docker.io/jwilder/dockerize \
