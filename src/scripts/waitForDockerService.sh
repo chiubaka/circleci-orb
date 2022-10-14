dc616e32a8d2fa2b1c0adc0c6ca4a677fbfb1a7a
Daniel Chiu <daniel@chiubaka.com>
2022-10-14 11:50:32 +0800

    Rever to using network attached to container directly

--- src/scripts/waitForDockerService.sh
+++ src/scripts/waitForDockerService.sh
@@ -3,12 +3,9 @@ set -e
 
 DOCKER_BINARY=${DOCKER_BINARY:-docker}
 
-# shellcheck disable=SC2016
-NETWORK_NAME=$($DOCKER_BINARY container inspect "$CONTAINER_NAME" --format \''{{range $net,$v := .NetworkSettings.Networks}}{{printf "%s" $net}}{{end}}'\')
-
 sleep 5
 
-$DOCKER_BINARY container run --network "$NETWORK_NAME" \
+$DOCKER_BINARY container run --network "container:$CONTAINER_NAME" \
   docker.io/jwilder/dockerize \
   -wait "$URL" \
   -wait-retry-interval "$RETRY_INTERVAL" \
