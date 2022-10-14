1988c1dcc0913f9cd992e534a1633c50742024fc
Daniel Chiu <daniel@chiubaka.com>
2022-10-14 11:44:13 +0800

    Add a delay before attempting to contact services

--- src/scripts/waitForDockerService.sh
+++ src/scripts/waitForDockerService.sh
@@ -6,6 +6,8 @@ DOCKER_BINARY=${DOCKER_BINARY:-docker}
 # shellcheck disable=SC2016
 NETWORK_NAME=$($DOCKER_BINARY container inspect "$CONTAINER_NAME" --format \''{{range $net,$v := .NetworkSettings.Networks}}{{printf "%s" $net}}{{end}}'\')
 
+sleep 5
+
 $DOCKER_BINARY container run --network "$NETWORK_NAME" \
   docker.io/jwilder/dockerize \
   -wait "$URL" \
