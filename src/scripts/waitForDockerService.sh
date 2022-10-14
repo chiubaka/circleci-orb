5d8e24c442e89d4f5b72972a386c1321486d059e
Daniel Chiu <daniel@chiubaka.com>
2022-10-14 11:22:05 +0800

    Fix over quoting / escaping issue in docker container inspect command

--- src/scripts/waitForDockerService.sh
+++ src/scripts/waitForDockerService.sh
@@ -3,7 +3,8 @@ set -e
 
 DOCKER_BINARY=${DOCKER_BINARY:-docker}
 
-NETWORK_NAME=$($DOCKER_BINARY container inspect "$CONTAINER_NAME" --format \'\{\{range \$net,\$v := .NetworkSettings.Networks\}\}\{\{printf \"%s\\n\" \$net\}\}\{\{end\}\}\')
+# shellcheck disable=SC2016
+NETWORK_NAME=$($DOCKER_BINARY container inspect "$CONTAINER_NAME" --format \''{{range $net,$v := .NetworkSettings.Networks}}{{printf "%s\n" $net}}{{end}}'\')
 
 $DOCKER_BINARY container run --network "$NETWORK_NAME" \
   docker.io/jwilder/dockerize \
