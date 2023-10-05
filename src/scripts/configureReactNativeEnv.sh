031d2b06235f149531a155b10400aeccd17bc913
Daniel Chiu <daniel@chiubaka.com>
2023-10-05 13:28:16 +0800

    Attempt to resolve shellcheck errors

--- src/scripts/configureReactNativeEnv.sh
+++ src/scripts/configureReactNativeEnv.sh
@@ -1,11 +1,14 @@
 #! /usr/bin/env bash
 set -e
 
-echo 'export PATH="$PATH:/usr/local/opt/node@$NODE_VERSION/bin:~/.yarn/bin:~/project/node_modules/.bin:~/project/example/node_modules/.bin"' >> $BASH_ENV
-echo 'export ANDROID_HOME="/usr/local/share/android-commandlinetools"' >> $BASH_ENV
-echo 'export ANDROID_SDK_ROOT="/usr/local/share/android-commandlinetools"' >> $BASH_ENV
-echo 'export PATH="$ANDROID_SDK_ROOT/emulator:$ANDROID_SDK_ROOT/tools:$ANDROID_SDK_ROOT/platform-tools:$PATH"' >> $BASH_ENV
-echo 'export QEMU_AUDIO_DRV=none' >> $BASH_ENV
-echo 'export JAVA_HOME=$(/usr/libexec/java_home)' >> $BASH_ENV
+{
+  echo 'export PATH="$PATH:/usr/local/opt/node@$NODE_VERSION/bin:~/.yarn/bin:~/project/node_modules/.bin:~/project/example/node_modules/.bin"'
+  echo 'export ANDROID_HOME="/usr/local/share/android-commandlinetools"'
+  echo 'export ANDROID_SDK_ROOT="/usr/local/share/android-commandlinetools"'
+  echo 'export PATH="$ANDROID_SDK_ROOT/emulator:$ANDROID_SDK_ROOT/tools:$ANDROID_SDK_ROOT/platform-tools:$PATH"'
+  echo 'export QEMU_AUDIO_DRV=none'
+  echo 'export JAVA_HOME=$(/usr/libexec/java_home)'
+} >> $BASH_ENV
 
+# shellcheck disable=1090
 source $BASH_ENV
