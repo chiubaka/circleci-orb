7d6921f23e861e65cd615e88ef0f543aa3706a54
Daniel Chiu <daniel@chiubaka.com>
2023-10-09 16:13:12 +0800

    Show output for sdkmanager commands

--- src/scripts/installAndroidEmulatorDependencies.sh
+++ src/scripts/installAndroidEmulatorDependencies.sh
@@ -1,8 +1,8 @@
 #! /usr/bin/env bash
 set -e
 
-yes | sdkmanager "platform-tools" "tools" > /dev/null
-yes | sdkmanager "platforms;$ANDROID_PLATFORM_VERSION" > /dev/null
-yes | sdkmanager "emulator" > /dev/null
-yes | sdkmanager "build-tools;$ANDROID_BUILD_TOOLS_VERSION" > /dev/null
-yes | sdkmanager --licenses > /dev/null
+yes | sdkmanager "platform-tools" "tools"
+yes | sdkmanager "platforms;$ANDROID_PLATFORM_VERSION"
+yes | sdkmanager "emulator"
+yes | sdkmanager "build-tools;$ANDROID_BUILD_TOOLS_VERSION"
+yes | sdkmanager --licenses
