a208d59e8e911bf23dbf8720fca290afd26b989f
Daniel Chiu <daniel@chiubaka.com>
2023-10-09 16:25:24 +0800

    Ignore exit code of yes command

--- src/scripts/installAndroidEmulatorDependencies.sh
+++ src/scripts/installAndroidEmulatorDependencies.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 set -e
 
-yes | sdkmanager "platform-tools" "tools"
-yes | sdkmanager "platforms;$ANDROID_PLATFORM_VERSION"
-yes | sdkmanager "emulator"
-yes | sdkmanager "build-tools;$ANDROID_BUILD_TOOLS_VERSION"
+(yes || true) | sdkmanager "platform-tools" "tools"
+(yes || true) | sdkmanager "platforms;$ANDROID_PLATFORM_VERSION"
+(yes || true) | sdkmanager "emulator"
+(yes || true) | sdkmanager "build-tools;$ANDROID_BUILD_TOOLS_VERSION"
