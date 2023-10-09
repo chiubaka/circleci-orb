1979ed3f68accb4c77791a9934af0a4b65598a02
Daniel Chiu <daniel@chiubaka.com>
2023-10-09 16:17:26 +0800

    Remove sdkmanager --licenses command

--- src/scripts/installAndroidEmulatorDependencies.sh
+++ src/scripts/installAndroidEmulatorDependencies.sh
@@ -5,4 +5,3 @@ yes | sdkmanager "platform-tools" "tools"
 yes | sdkmanager "platforms;$ANDROID_PLATFORM_VERSION"
 yes | sdkmanager "emulator"
 yes | sdkmanager "build-tools;$ANDROID_BUILD_TOOLS_VERSION"
-yes | sdkmanager --licenses
